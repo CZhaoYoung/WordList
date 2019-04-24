0791a7d5ff5695d9c545a43b480d844ca830aff6
Rlee063 <877624892@qq.com>
2019-04-24 14:59:10 +0800

    finish logic in lirary

--- miniprogram/pages/library/library.js
+++ miniprogram/pages/library/library.js
@@ -5,19 +5,47 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    booksList : []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.refresh()
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
+  refresh: function() {
+    var cBookInfo = {
+      name: "六级考纲词汇",
+      plan: {
+        new: 200,
+        old: 400
+      },
+      totalNum: 2340,
+      studiedNum: 242,
+      imgUrl: "../../images/books/book1.png",
+      star: true
+    }
+
+    var booksList = []
+    booksList.push(cBookInfo)
+    cBookInfo.name = "四级词汇"
+    booksList.push(cBookInfo)
+    this.setData({
+      booksList: booksList
+    })
+  },
+
+  star: function(e){
+    var booksList = this.data.booksList
+    var index = e.currentTarget.dataset.index
+    booksList[index].star = !booksList[index].star
+    this.setData({
+      booksList: booksList
+    })
+  },
+
   onReady: function () {
 
   },
