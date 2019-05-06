05903d8b51a4b4f98fd15ddf6335c8a71d04b5dc
Rlee063 <877624892@qq.com>
2019-05-06 21:44:48 +0800

    finish calendar

--- miniprogram/pages/calendar/calendar.js
+++ miniprogram/pages/calendar/calendar.js
@@ -4,21 +4,80 @@ Page({
     month: 12,
     date: ['日', '一', '二', '三', '四', '五', '六'],
     dateArr: [],
+    monthArr: [],
     isToday: 0,
     isTodayWeek: false,
-    todayIndex: 0
+    todayIndex: 0,
+    signed:[
+      {
+        year: 2019,
+        month: 4,
+        day: 24
+      },
+      {
+        year: 2019,
+        month: 3,
+        day: 20
+      }
+    ]
   },
   onLoad: function () {
-    let now = new Date();
-    let year = now.getFullYear();
-    let month = now.getMonth() + 1;
-    this.dateInit();
+    this.monthInit()
+    this.statusInit()
     console.log(this.data)
+  },
+  monthInit: function(){
+    var monthArr = []
+    var now = new Date()
+    var y = now.getFullYear()
+    var m = now.getMonth()
+    for(var i=0; i<5; i++){
+      var month = {}
+      month.dateArr = this.dateInit(y, m)
+      month.year = y
+      month.month = m
+      monthArr.push(month)
+      y = m - 1 < 0 ? y - 1 : y;
+      m = m - 1 < 0 ? 11 : m - 1;
+    }
     this.setData({
-      year: year,
-      month: month,
-      isToday: '' + year + month + now.getDate()
+      monthArr: monthArr
     })
+    console.log(monthArr)
+  },
+  statusInit: function(){
+    var now = new Date()
+    var y = now.getFullYear()
+    var m = now.getMonth()
+    var d = now.getDate()
+    console.log(d)
+    var monthArr = this.data.monthArr
+
+    for(let j of monthArr){
+      if(y != j.year) continue;
+      if(m != j.month) continue;
+      else{
+        var k = 0
+        while(!(j.dateArr[k].dateNum)) k++
+        j.dateArr[k-1+d].status = 2
+      }
+    }
+
+    for(let i of this.data.signed){
+        for(let j of monthArr){
+          if(i.year != j.year) continue;
+          if(i.month != j.month) continue;
+          else{
+            var k = 0
+            while(!(j.dateArr[k].dateNum)) k++
+            j.dateArr[k-1+i.day].status = 1
+          }
+        }
+    }
+    this.setData({
+      monthArr: monthArr
+    })
+    console.log(monthArr)
   },
   dateInit: function (setYear, setMonth) {
     //全部时间的月份都是按0~11基准，显示月份才+1
@@ -42,7 +101,6 @@ Page({
       if (i >= startWeek) {
         num = i - startWeek + 1;
         obj = {
-          isToday: '' + year + (month + 1) + num,
           dateNum: num,
           weight: 5
         }
@@ -54,48 +112,6 @@ Page({
     this.setData({
       dateArr: dateArr
     })
-    let nowDate = new Date();
-    let nowYear = nowDate.getFullYear();
-    let nowMonth = nowDate.getMonth() + 1;
-    let nowWeek = nowDate.getDay();
-    let getYear = setYear || nowYear;
-    let getMonth = setMonth >= 0 ? (setMonth + 1) : nowMonth;
-    if (nowYear == getYear && nowMonth == getMonth) {
-      this.setData({
-        isTodayWeek: true,
-        todayIndex: nowWeek
-      })
-    } else {
-      this.setData({
-        isTodayWeek: false,
-        todayIndex: -1
-      })
-    }
-  },
-  /**
-   * 上月切换
-   */
-  lastMonth: function () {
-    //全部时间的月份都是按0~11基准，显示月份才+1
-    let year = this.data.month - 2 < 0 ? this.data.year - 1 : this.data.year;
-    let month = this.data.month - 2 < 0 ? 11 : this.data.month - 2;
-    this.setData({
-      year: year,
-      month: (month + 1)
-    })
-    this.dateInit(year, month);
-  },
-  /**
-   * 下月切换
-   */
-  nextMonth: function () {
-    //全部时间的月份都是按0~11基准，显示月份才+1
-    let year = this.data.month > 11 ? this.data.year + 1 : this.data.year;
-    let month = this.data.month > 11 ? 0 : this.data.month;
-    this.setData({
-      year: year,
-      month: (month + 1)
-    })
-    this.dateInit(year, month);
+    return dateArr
   }
 })
