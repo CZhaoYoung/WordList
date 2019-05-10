867aab15417e97beddf83040a7234f2398eaa3c5
Rlee063 <877624892@qq.com>
2019-05-10 17:16:24 +0800

    完成wordInfo逻辑搭建 未协调接口

--- miniprogram/pages/wordInfo/wordInfo.js
+++ miniprogram/pages/wordInfo/wordInfo.js
@@ -1,81 +1,116 @@
-// miniprogram/pages/wordInfo/wordInfo.js
+// miniprogram/pages/main/main.js
 var utils = require("../../utils/utils");
-var that
+var that;
 
 Page({
+
   /**
    * 页面的初始数据
    */
   data: {
-    pattern: 1,
-    sentenceString : "The fresh air of the September morning braced him.",
-    sentence: []
+    wordInfo: [],
+    queryWordInfo: [],
+    isQuery: false
   },
+
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
     that = this;
-    this.setData({
-      sentence : utils.parseSentence(this.data.sentenceString, "braced")
+    wx.cloud.callFunction({
+      name: 'getWordInfo',
+      data: {
+        name: options.Wid
+      },
+      success: res => {
+        if(!res.result.valid){
+          wx.showToast({
+            title: '无该单词信息',
+            icon: 'none',
+            duration: 2000
+          })
+          setTimeout(()=>{
+            wx.navigateBack({
+              delta: 1
+            })
+          },2000)
+        }
+        that.setData({
+          wordInfo: res.result.wordInfo
+        })
+        this.initMagicSentence();
+      },
+      fail: err => {
+        wx.showToast({
+          title: '获取单词信息失败',
+          icon: 'none',
+          duration: 2000
+        })
+      }
     })
-    console.log(this.data.sentence);
-  },
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
 
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
   onUnload: function () {
-
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
+  pronounce: function () {
+    const innerAudioContext = wx.createInnerAudioContext()
+    innerAudioContext.autoplay = true
+    innerAudioContext.src = that.data.wordInfo.audioUrl
+    innerAudioContext.onPlay(() => { })
   },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  },
-
-  selectWord: function(e) {
-    console.log(e);
-    var sentence = this.data.sentence;
-    sentence[e.currentTarget.dataset.index].selected = !sentence[e.currentTarget.dataset.index].selected;
+  selectWord: function (e) {
+    var wordInfo = this.data.wordInfo;
+    // wordInfo.magicSentence[e.currentTarget.dataset.index].selected = !wordInfo.magicSentence[e.currentTarget.dataset.index].selected;
+    var word;
+    for (var i = 0; i < wordInfo.magicSentence.length; i++) {
+      if (i != e.currentTarget.dataset.index) {
+        wordInfo.magicSentence[i].selected = false;
+      }
+      else {
+        if (wordInfo.magicSentence[i].selected != true) {
+          wordInfo.magicSentence[i].selected = true;
+          word = wordInfo.magicSentence[i].word;
+        }
+        else {
+          return
+        }
+      }
+    }
+    // console.log("query="+word)
+    //查询单词等待回调
     that.setData({
-      sentence: sentence
+      queryWordInfo: wordInfo,
+      wordInfo: wordInfo,
+      isQuery: true
     })
+  },
+
+  exitQuery: function (e) {
+    var wordInfo = this.data.wordInfo;
+    for (var i = 0; i < wordInfo.magicSentence.length; i++) {
+      wordInfo.magicSentence[i].selected = false
+    }
+    that.setData({
+      wordInfo: wordInfo,
+      isQuery: false
+    })
+  },
+
+  initMagicSentence: function () {
+    var wordInfo = this.data.wordInfo
+    wordInfo.magicSentence = utils.parseSentence(this.data.wordInfo.sentence, this.data.wordInfo.name)
+    this.setData({
+      wordInfo: wordInfo
+    })
+  },
+
+  nextHandle: function (e) {
+    that.updateWordInfo()
+    that.changePattern()
+    that.updateTopBar()
   }
 })
