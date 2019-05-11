e89ea4b09d4d524ef91c53228926a97529e4acde
Rlee063 <877624892@qq.com>
2019-05-11 17:21:51 +0800

    获取每日任务

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -2,7 +2,6 @@
 var data = require('utils/data.js')
 App({
   onLaunch: function () {
-    
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
@@ -48,14 +47,31 @@ App({
   },
 
   setNewTask: function() {
-    var task = data.getFakeTask();
-    wx.setStorageSync('task', task);
-    this.resetProgress(task);
+    console.log("here")
+    wx.cloud.callFunction({
+      name: 'getDailyTask',
+      data: {
+      },
+      success: res => {
+        console.log(res)
+        wx.setStorageSync('task', res.result.task)
+        this.resetProgress(res.result.task);
+      },
+      fail: err => {
+        console.log(err)
+        wx.showToast({
+          title: '获取任务失败',
+          icon: 'none',
+          duration: 2000
+        })
+      }
+    })
   },
 
   resetProgress: function(task) {
     var progress = {
       globalTimeCount: 0,
+      localTimeCount: 0,
       complete: false,
       totalNum: 0,
       unstudyWords: [],
@@ -67,6 +83,7 @@ App({
     progress.totalNum = task.newWords.length;
     progress.unstudyWords = task.newWords;
     wx.setStorageSync('newWordsProgress', progress);
+    progress.totalNum = task.oldWords.length;
     progress.unstudyWords = task.oldWords;
     wx.setStorageSync('oldWordsProgress', progress);
   }
